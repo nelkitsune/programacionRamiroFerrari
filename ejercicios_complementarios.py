566cd2233ce694b82f31141e1f31a5cad57e4e7f
unknown <nelkitsune>
2023-08-16 15:30:59 -0300

    primeros trabajos

---
+++ ejercicios_complementarios.py
@@ -0,0 +1,57 @@
+#Ramiro Ferrari comision3 TUP
+
+#punto 1
+numero1 = 16
+#punto2
+numero2= 3.14
+#punto 3
+suma = numero1 + numero2
+#punto 4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(suma," ",resta," ",multiplicacion," ",division)
+#punto 5
+nombre = "Ramiro"
+#punto 6
+precio = 999.99
+#punto 7
+descuento= 0.75
+#punto 8
+precio_final = precio * descuento
+#print(precio_final)
+#punto 9
+cadena = "Buenos días y a trabajar, que nacimos estupendas pero no millonaria"
+#punto 10
+#print(len(cadena))
+longitud = len(cadena)
+print(longitud)
+#punto11
+precio_2 = 3.14159265
+precio_2 = int(precio_2)
+#print(precio_2)
+#punto 12
+nombre_2 = "Ramiro"
+apellido = "Ferrari"
+nombre_completo = nombre_2 + " " + apellido
+#print(nombre_completo)
+#punto 13
+edad = 21
+edad +=5
+edad -=10
+#print(edad)
+#punto 14
+altura = 1.90
+altura *=4
+altura /=3
+#print(altura)
+#punto 15
+nombre_3 = "RAMIRO"
+nombre_3 = nombre_3.lower()
+#print(nombre_3)
+#punto16
+nombre_3 = "RAMIRO"
+primer_letra = nombre_3[0].lower()
+resto_texto = nombre_3[1:]
+texto_final = primer_letra + resto_texto
+print(texto_final)
