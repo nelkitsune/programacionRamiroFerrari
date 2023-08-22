08dfcdd98df4c26c2fd479beef290b590b8117b7
unknown <nelkitsune>
2023-08-22 10:52:43 -0300

    ahora si listo el tp1-2

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,18 @@
-#21
-kml = int(input("ingrese la cantidad de KM que puede recorrer su moto por cada litro de combustible: "))
-capaciodad = int(input("ingrese cuantos litros puede almacenar su moto: "))
-distancia = int(input("ingrese la distancia que debe recorrer: "))
-resultado_21 = distancia / (kml * capaciodad)
-print(resultado_21)
+
+#19
+#dia = int(input("porfavor ingrese su dia de nacimiento en numeros: "))
+#mes = int(input("porfavor ingrese su dia de mes en numeros: "))
+#ano = int(input("porfavor ingrese su dia de años en numeros: "))
+#print(f"la fecha de nacimiento es: {dia}/{mes}/{ano}")
+fecha_a = input("ingrese la fecha de nacimiento con el siguiente formato: dia / mes / año : ")
+print(f"la fecha de nacimiento es: {fecha_a}")
+
+#-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+
+#20
+#dia = input("porfavor ingrese su dia de nacimiento en numeros: ")
+#mes = input("porfavor ingrese su dia de mes en numeros: ")
+#ano = input("porfavor ingrese su dia de años en numeros: ")
+#fecha = dia + "" + mes + "" + ano
+fecha_b = int(input("ingrese su fecha de nacimineto toda junta sin espacios con el siguiente formato: ddmmaaaa    "))
+print(f"la fecha de nacimiento es: {fecha_b}")
--- tp1_parte2.py
+++ tp1_parte2.py
@@ -201,19 +201,22 @@ print(coste_f)
 #-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 #19
-dia = int(input("porfavor ingrese su dia de nacimiento en numeros: "))
-mes = int(input("porfavor ingrese su dia de mes en numeros: "))
-ano = int(input("porfavor ingrese su dia de años en numeros: "))
-print(f"la fecha de nacimiento es: {dia}/{mes}/{ano}")
+#dia = int(input("porfavor ingrese su dia de nacimiento en numeros: "))
+#mes = int(input("porfavor ingrese su dia de mes en numeros: "))
+#ano = int(input("porfavor ingrese su dia de años en numeros: "))
+#print(f"la fecha de nacimiento es: {dia}/{mes}/{ano}")
+fecha_a = input("ingrese la fecha de nacimiento con el siguiente formato: dia / mes / año : ")
+print(f"la fecha de nacimiento es: {fecha_a}")
 
 #-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 #20
-dia = input("porfavor ingrese su dia de nacimiento en numeros: ")
-mes = input("porfavor ingrese su dia de mes en numeros: ")
-ano = input("porfavor ingrese su dia de años en numeros: ")
-fecha = dia + "" + mes + "" + ano
-print(f"la fecha de nacimiento es: {fecha}")
+#dia = input("porfavor ingrese su dia de nacimiento en numeros: ")
+#mes = input("porfavor ingrese su dia de mes en numeros: ")
+#ano = input("porfavor ingrese su dia de años en numeros: ")
+#fecha = dia + "" + mes + "" + ano
+fecha_b = int(input("ingrese su fecha de nacimineto toda junta sin espacios con el siguiente formato: ddmmaaaa    "))
+print(f"la fecha de nacimiento es: {fecha_b}")
 
 #-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
