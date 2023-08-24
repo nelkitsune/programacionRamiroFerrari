09cfc302b15c701014a13682f5c641162c1a73fe
RamiroFerrari <ramiroferrariadrian@gmail.com>
2023-08-24 10:17:12 -0300

    coso

--- ejercicio_clase_22I8.py
+++ ejercicio_clase_22I8.py
@@ -2,9 +2,9 @@ fecha = input("ingrese la fecha en el siguiente formato dia, dd/mm ")
 dia_semana = fecha[ 0 : fecha.find(",")]
 dia_fecha = int(fecha[ fecha.find(" ") : fecha.find("/")])
 mes_fecha = int(fecha [ fecha.find("/")+1:])
-dia_semana = dia_semana.lower()
+dia_semana_low = dia_semana.lower()
 
-if dia_semana != ("lunes" or "martes" or "miercoles" or "jueves" or "viernes" or "sabado" or "domingo"):
+if dia_semana_low != "lunes" or dia_semana_low != "martes" or dia_semana_low != "miercoles" or dia_semana_low != "jueves" or dia_semana_low != "viernes":
     print("dia de la semana incorrecto, reinicie el programa")
 else:
     if dia_fecha > 31 :
@@ -12,3 +12,4 @@ else:
     else:
         if dia_fecha > 31 :
             print("fecha incorrecta, reinicie el programa")
+            
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-fecha = input("ingrese la fecha en el siguiente formato dia, dd/mm ")
-dia_semana = fecha[ 0 : fecha.find(",")]
-dia_fecha = fecha[ fecha.find(" ") : fecha.find("/")]
-mes_fecha = fecha [ fecha.find("/")+1:]
-print(dia_semana + "/" + dia_fecha + "/" + mes_fecha)
