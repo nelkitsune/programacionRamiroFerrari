fa44fea9936c72048f6162da9eece0876bf87141
RamiroFerrari <ramiroferrariadrian@gmail.com>
2023-08-31 11:48:49 -0300

    avantp3

---
+++ tp3.py
@@ -0,0 +1,22 @@
+#1)
+#name = input("ingrese el nombre: ")
+#for i in range(10):
+#    print(name)
+
+#2)
+
+#age = int(input("ingrese su edad: "))
+#for i in range(age):
+#    print(i+1)
+
+#3)
+aux =""
+num1 = int(input("ingrese un numero"))
+for i in range(num1):
+    if  i % 2 != 0:
+        aux2= str(i)
+        aux = str (aux +", " + aux2)
+
+print(aux)
+
+#4)
