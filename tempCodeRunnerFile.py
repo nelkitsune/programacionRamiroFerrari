df7a9aab36122369ad678a3c86e59b7ed2b0a502
RamiroFerrari <ramiroferrariadrian@gmail.com>
2023-09-05 16:58:43 -0300

    avance tarea tp3

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-horas=int(input("ingrese la cantidad de horas trabajadas: "))
-salario_hora = int ( input ("ingrese el salario por hora: "))
-if horas > 48 :
-    horas_final = 48 - horas
-    horas -=horas_final
-    salario_final = (salario_hora * horas) + ((salario_hora * horas_final) * 1.10)
-else:
-    salario_final = salario_hora * horas
-print(f"el salario final es de ${salario_final}")
--- tp3.py
+++ tp3.py
@@ -10,13 +10,20 @@
 #    print(i+1)
 
 #3)
-aux =""
-num1 = int(input("ingrese un numero"))
-for i in range(num1):
-    if  i % 2 != 0:
-        aux2= str(i)
-        aux = str (aux +", " + aux2)
-
-print(aux)
+#aux =""
+#num1 = int(input("ingrese un numero"))
+#for i in range(num1):
+#    if  i % 2 != 0:
+#        aux2= str(i)
+#        aux = str (aux +", " + aux2)
+#
+#print(aux)
 
 #4)
+#auxiliary2=""
+#num2 = int(input("ingrese un numeor entero positivo: "))
+#for i in range(num2, 0 , -1):
+#    auxiliary3 = str(i)
+#    auxiliary2 = str(auxiliary2 + ", " + auxiliary3)
+#print(auxiliary2)
+#5)
