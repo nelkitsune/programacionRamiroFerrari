1e3f32f33a855bb17229a7d27c7ecddc71f9700b
nelkitsune <ramiroferrariadrian@gmail.com>
2023-10-21 16:45:11 -0300

    tp7

--- funcion_recursiva_tarea.py
+++ funcion_recursiva_tarea.py
@@ -1,10 +1,13 @@
 import random
-def rat(option, time):
-    if option == 3:
-        return 7
-    elif option == 2:
-        time = time + rat(random.randint(1,3),time)
-        return 3
-    elif option == 1 : 
-        time = time + rat(random.randint(1,3),time)
-        return 5
+def rat():
+    time = 0
+    while True:
+        elec = random.randint(1, 3)  # Elegir un camino al azar
+        if elec == 1:
+            time += 3
+        elif elec == 2:
+            time += 5
+        else:  # eleccion == 3
+            time += 7
+            break  # La rata ha salido de la jaula
+    return time
--- recursividad_tarea.py
+++ recursividad_tarea.py
@@ -1,12 +1,5 @@
 import funcion_recursiva_tarea
-import random
-val = random.randint(1,3)
-if val == 1:
-    time = 3
-    print(f"se tardo un total de {funcion_recursiva_tarea.rat(random.randint(1,3),time)} minutos")
-elif val == 2:
-    time = 5
-    print(f"se tardo un total de {funcion_recursiva_tarea.rat(random.randint(1,3),time)} minutos")
-elif val == 3:
-    time = 7
-    print("tardo 7 minutos")
+time = funcion_recursiva_tarea.rat()
+print(f"La rata salió de la jaula en {time} minutos.")
+
+#realizar una función que  utiliza recursión para realizar una inversión de dígitos
