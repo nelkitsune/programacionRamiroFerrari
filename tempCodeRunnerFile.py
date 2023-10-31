0d5e9066b816dc0fe12c6701dfa3f8365fd02947
RamiroFerrari <ramiroferrariadrian@gmail.com>
2023-10-31 11:56:09 -0300

    termi

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,84 +1,37 @@
-class Contact:
-    def __init__(self, name, phone, email):
-        self.name = name
-        self.phone = phone
-        self.email = email
-class Agenda:
-    def __init__(self):
-        self.contacts = []
-    def add_contact(self, name, phone, email):
-        new_contact = Contact(name, phone, email)
-        self.contacts.append(new_contact)
-        print("Contacto agregado con éxito.")
-    def list_contacts(self):
-        if not self.contacts:
-            print("La agenda está vacía.")
-        else:
-            print("Lista de contactos:")
-            for contact in self.contacts:
-                print(f"Nombre: {contact.name}")
-                print(f"Teléfono: {contact.phone}")
-                print(f"Email: {contact.email}")
-                print("")
-    def find_contact(self, name):
-        found = False
-        for contact in self.contacts:
-            if contact.name == name:
-                print(f"Contacto encontrado:")
-                print(f"Nombre: {contact.name}")
-                print(f"Teléfono: {contact.phone}")
-                print(f"Email: {contact.email}")
-                found = True
-                break
-        if not found:
-            print(f"El contacto '{name}' no se encuentra en la agenda.")
-    def edit_contact(self, name, new_phone, new_email):
-        found = False
-        for contact in self.contacts:
-            if contact.name == name:
-                contact.phone = new_phone
-                contact.email = new_email
-                print(f"Contacto '{name}' editado con éxito.")
-                found = True
-                break
-        if not found:
-            print(f"El contacto '{name}' no se encuentra en la agenda.")
-    def close_agenda(self):
-        print("Agenda cerrada.")
-def menu():
-    print("Menú de la Agenda:")
-    print("1. Añadir contacto")
-    print("2. Lista de contactos")
-    print("3. Buscar contacto")
-    print("4. Editar contacto")
-    print("5. Cerrar agenda")
-    option = input("Seleccione una opción (1/2/3/4/5): ")
-    return option
+goals = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 0, 1, 2, 3, 4, 5, 6, 7, 8],
+    [2, 1, 0, 1, 2, 3, 4, 5, 6, 7],
+    [3, 2, 1, 0, 1, 2, 3, 4, 5, 6],
+    [4, 3, 2, 1, 0, 1, 2, 3, 4, 5],
+    [5, 4, 3, 2, 1, 0, 1, 2, 3, 4],
+    [6, 5, 4, 3, 2, 1, 0, 1, 2, 3],
+    [7, 6, 5, 4, 3, 2, 1, 0, 1, 2],
+    [8, 7, 6, 5, 4, 3, 2, 1, 0, 1],
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+]
 
+wins = [0] * 10
+draws = [0] * 10
+losses = [0] * 10
+goals_scored = [0] * 10
+goals_conceded = [0] * 10
 
+for i in range(10):
+    for j in range(10):
+        if i != j:
+            if goals[i][j] > goals[j][i]:
+                wins[i] += 1
+            elif goals[i][j] < goals[j][i]:
+                losses[i] += 1
+            else:
+                draws[i] += 1
+            goals_scored[i] += goals[i][j]
+            goals_conceded[i] += goals[j][i]
 
+goal_difference = [goals_scored[i] - goals_conceded[i] for i in range(10)]
+points = [wins[i] * 3 + draws[i] for i in range(10)]
 
-agenda = Agenda()
-
-while True:
-    option = menu()
-    if option == "1":
-        name = input("Ingrese el nombre del contacto: ")
-        phone = input("Ingrese el teléfono del contacto: ")
-        email = input("Ingrese el correo electrónico del contacto: ")
-        agenda.add_contact(name, phone, email)
-    elif option == "2":
-        agenda.list_contacts()
-    elif option == "3":
-        name = input("Ingrese el nombre del contacto a buscar: ")
-        agenda.find_contact(name)
-    elif option == "4":
-        name = input("Ingrese el nombre del contacto a editar: ")
-        new_phone = input("Ingrese el nuevo teléfono del contacto: ")
-        new_email = input("Ingrese el nuevo correo electrónico del contacto: ")
-        agenda.edit_contact(name, new_phone, new_email)
-    elif option == "5":
-        agenda.close_agenda()
-        break
-    else:
-        print("Opción no válida. Por favor, seleccione una opción válida (1/2/3/4/5).")
+for i in range(10):
+    print(f"Equipo {i + 1}: Victorias: {wins[i]}, Empates: {draws[i]}, Derrotas: {losses[i]}, Diferencia de goles: {goal_difference[i]}, Puntos: {points[i]}")
+#9
--- tp6.py
+++ tp6.py
@@ -107,18 +107,42 @@ print("\nCantidad total de ocurrencias de cada carácter:")
 for caracter, cantidad in character_count.items():
     print(f"'{caracter}': {cantidad}")
 #8
-championship = [
-    [0,4,1,0,3,3,2,1,3,2]
-    [0,0,2,3,1,0,0,2,3,4]
-    [2,1,0,0,2,2,0,4,2,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0]
-    ]
+goals = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 0, 1, 2, 3, 4, 5, 6, 7, 8],
+    [2, 1, 0, 1, 2, 3, 4, 5, 6, 7],
+    [3, 2, 1, 0, 1, 2, 3, 4, 5, 6],
+    [4, 3, 2, 1, 0, 1, 2, 3, 4, 5],
+    [5, 4, 3, 2, 1, 0, 1, 2, 3, 4],
+    [6, 5, 4, 3, 2, 1, 0, 1, 2, 3],
+    [7, 6, 5, 4, 3, 2, 1, 0, 1, 2],
+    [8, 7, 6, 5, 4, 3, 2, 1, 0, 1],
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+]
+
+wins = [0] * 10
+draws = [0] * 10
+losses = [0] * 10
+goals_scored = [0] * 10
+goals_conceded = [0] * 10
+
+for i in range(10):
+    for j in range(10):
+        if i != j:
+            if goals[i][j] > goals[j][i]:
+                wins[i] += 1
+            elif goals[i][j] < goals[j][i]:
+                losses[i] += 1
+            else:
+                draws[i] += 1
+            goals_scored[i] += goals[i][j]
+            goals_conceded[i] += goals[j][i]
+
+goal_difference = [goals_scored[i] - goals_conceded[i] for i in range(10)]
+points = [wins[i] * 3 + draws[i] for i in range(10)]
+
+for i in range(10):
+    print(f"Equipo {i + 1}: Victorias: {wins[i]}, Empates: {draws[i]}, Derrotas: {losses[i]}, Diferencia de goles: {goal_difference[i]}, Puntos: {points[i]}")
 #9
 import random
 existe_valor = True
